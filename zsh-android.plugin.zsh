404ca367344ef8e003f8d0ecf7d593c900b8f01f
ri7nz <ri7nz@evilfactory.id>
2021-04-14 18:37:50 +0700

    feat: minimal no-setup for sdkmanager
    NOTES:
    
    - install cmdline-tools (sdkmanager)
    - agree licenses
    - java8 (only OSX/Darwin)

--- zsh-android.plugin.zsh
+++ zsh-android.plugin.zsh
@@ -1,23 +1,40 @@
+reset="\033[0m"
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+cyan="\033[36m"
+white="\033[37m"
+
 _install_packages() {
-    # define $ANDROID_SDK_ROOT
-    export ANDROID_SDK_ROOT=$ANDROID_HOME
     local DEFAULT_PACKAGES;
     local PACKAGES;
+    # currently only work in `mac` for auto install openjdk8
+    # TODO support linux-based for openjdk8 
+    command -v brew &>/dev/null && {
+      printf "$yellow > checking jdk8...$reset\n"
+      HOMEBREW_NO_AUTO_UPDATE=1 brew list --cask adoptopenjdk8 || {
+        printf "$cyan > start to install adoptopenjdk8...$reset\n"
+        brew tap adoptopenjdk/openjdk
+        brew install --cask adoptopenjdk8
+      }
+      printf "$green > already installed jdk8 $reset\n"
+    }
     DEFAULT_PACKAGES="platform-tools platforms;android-29 build-tools;29.0.2 emulator"
     # define $PACKAGES and install with sdkmanager
     # fallback to $DEFAULT_PACKAGES when null for $ZSH_ANDROID_PACKAGES
     PACKAGES="${ZSH_ANDROID_PACKAGES:-$DEFAULT_PACKAGES}"
-    echo "install " $PACKAGES
+    printf "$red> install $PACKAGES ...$reset\n"
     # Eyden I. — 'Arguing with a woman is 
     # like reading the Software License Agreement. 
     # In the end, you ignore everything and click I agree.'
-    yes | sdkmanager --licenses --sdk_root=$ANDROID_SDK_ROOT
+    yes | sdkmanager --licenses 
     # start to install $PACKAGES with sdkmanager
-    sdkmanager --install $PACKAGES  --sdk_root=$ANDROID_SDK_ROOT
+    sdkmanager --update
     unset -f DEFAULT_PACKAGES PACKAGES
   }
 
 _install_cmdline() {
+    printf "$green > (current) cmdline-tools version: $1 $reset\n"
     local machine
     local cmdlinetools_url;
     local cmdlinetools_file;
@@ -39,47 +56,58 @@ _install_cmdline() {
         ;;
     esac 
 
-    cmdlinetools_file="commandlinetools-$machine-6858069_latest.zip"
+    cmdlinetools_file="commandlinetools-$machine-$1.zip"
     cmdlinetools_url="https://dl.google.com/android/repository/$cmdlinetools_file"
     tmp_cmdlinetools_file=/tmp/$cmdlinetools_file
     
-    -e $tmp_cmdlinetools_file &>/dev/null && {
-     curl --continue-at - $cmdlinetools_url -o $tmp_cmdlinetools_file 
+    test -f $tmp_cmdlinetools_file && {
+     test -f $tmp_cmdlinetools_file.cache || {
+      curl --continue-at - $cmdlinetools_url -o $tmp_cmdlinetools_file 
+     }
     } || {
+     printf "$yellow> start to download $cmdlinetools_file...$reset\n"
      curl -o $tmp_cmdlinetools_file $cmdlinetools_url
     }
-
-    rm -rf $ANDROID_HOME/cmdline-tools
-    unzip $tmp_cmdlinetools_file -d $ANDROID_HOME
- 
-    unset -f machine cmdlinetools_file cmdlinetools_url tmp_cmdlinetools_file _install_cmdline
+    printf "$yellow> extracting cmdline-tools version $1 in $ANDROID_HOME/cmdline-tools/$1...$reset\n"
+    unzip -o  $tmp_cmdlinetools_file -d  /tmp
+    mkdir -p $ANDROID_HOME/cmdline-tools/$1/
+    cp -r /tmp/cmdline-tools/* $ANDROID_HOME/cmdline-tools/$1/
+    rm -rf /tmp/cmdline-tools
+    # create flag in /tmp
+    touch $tmp_cmdlinetools_file.cache
+    printf "$green> success extract in $ANDROID_HOME/cmdline-tools/$1 $reset\n"
+    printf "$yellow> make it as default with symlink...$reset\n"
+    ln -sf $ANDROID_HOME/cmdline-tools/$1 $ANDROID_HOME/cmdline-tools/default
+    printf "$green> sucess make it default $reset\n"
 }
 
--v ANDROID_HOME &>/dev/null && {
-  setopt verbose
-  # when $ANDROID_HOME exist just export PATH
-  export PATH=$ANDROID_HOME/cmdline-tools/bin/:$PATH
-  export PATH=$ANDROID_HOME/emulator/:$PATH
-  export PATH=$ANDROID_HOME/platform-tools/:$PATH 
-} || {
-  # define $ANDROID_HOME {default: $HOME/android}
-  # make directory $ANDROID_HOME when isn't exist
-  -d $ANDROID_HOME &>/dev/null || {
-    mkdir -p $ANDROID_HOME
+command -v sdkmanager &>/dev/null || {
+  export ANDROID_HOME="${ZSH_ANDROID_HOME:-$HOME/android}"
+  export ANDROID_SDK_ROOT=$ANDROID_HOME/sdk
+  export PATH="$ANDROID_HOME/cmdline-tools/default/bin:$PATH"
+  export PATH="$ANDROID_HOME/emulator:$PATH"
+  export PATH="$ANDROID_HOME/platform-tools:$PATH"
+  export PATH="$ANDROID_HOME/tools:$PATH"
+  version=${ANDROID_CMDLINE_VERSION:-"6858069_latest"}
+  printf"$cyan > start to install...$reset\n"
+  test -d $ANDROID_SDK_ROOT || {
+    printf "$cyan > created \$ANDROID_SDK_ROOT in $ANDROID_ROOT $reset\n"
+    mkdir -p $ANDROID_SDK_ROOT
   }
-  export ANDROID_HOME=$HOME/android
-  # posibly bug when we extract cmdline-tools.zip and folder name not same with `cmdline-tools`
-  export PATH=$ANDROID_HOME/cmdline-tools/bin:$PATH
-  export PATH=$ANDROID_HOME/emulator:$PATH
-  export PATH=$ANDROID_HOME/platform-tools:$PATH 
-  # currently only work in `mac` for auto install openjdk8
-  # TODO support linux-based for openjdk8 
-  command -v brew &>/dev/null && {
-    $(brew list --cask | grep adoptopenjdk/openjdk/adoptopenjdk8) &>/dev/null || {
-      brew install --cask adoptopenjdk/openjdk/adoptopenjdk8
+  test -d $ANDROID_HOME || {
+      printf "$cyan > created \$ANDROID_HOME in $ANDROID_HOME $reset\n"
+      mkdir -p $ANDROID_HOME
+      test -d $ANDOID_HOME/cmdline-tools || {
+        printf "$cyan > created cmdline-tools in $ANDROID_HOME/cmdline-tools $reset\n"
+        mkdir -p $ANDROID_HOME/cmdline-tools
+      }
     }
-  }
-  command -v sdkmanager &>/dev/null || _install_cmdline 
-  command -v sdkmanager &>/dev/null && _install_packages 
-  unsetopt verbose
+  command -v sdkmanager &>/dev/null || _install_cmdline  $version
+  test -d $ANDROID_HOME/licenses || _install_packages
+  # TODO:next
+  # 1) sdkmanager installation
+  # 2) agree licenses of sdkmanager
+  # #) make standalone function for java8
 }
+
+unset -f _install_packages _install_cmdline
